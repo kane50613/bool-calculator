ae4eee8dbaff305d5d2250134cd0763297f72a48
Gary50613 <50613gary@gmail.com>
2021-09-09 19:46:43 +0800

    finished calculate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const $ = mdui.$
 const charFrom = 65
+const input = $('#calc').get(0)
 let vars = []
 
 $('#add-variable').get(0).addEventListener('click', () => {
@@ -10,9 +11,11 @@ $('#add-variable').get(0).addEventListener('click', () => {
     button.classList.add('variable')
     button.innerText = vars[vars.length - 1]
     button.addEventListener('click', () => {
-        $('#calc').get(0).value += button.innerText
+        input.value += button.innerText
+        update()
     })
     $('#vars').get(0).appendChild(button)
+    update()
     // mdui.prompt(
     //     '新增變數',
     //     (value) => {
@@ -38,6 +41,7 @@ $('#remove-variable').get(0).addEventListener('click', () => {
     if(vars.length === 0)
         return
     vars.splice(vars.length - 1, 1)
+    update()
     // let vars_text = vars.map(x => `<li id="remove-var--${x}" class="mdui-list-item mdui-ripple"><div class="mdui-list-item-content">${x}</div><i class="mdui-icon material-icons">&#xe5cd;</i></li>`)
     // mdui.dialog({
     //     title: '移除變數',
@@ -56,17 +60,27 @@ $('#remove-variable').get(0).addEventListener('click', () => {
     // })
 })
 
-$('#calc').get(0).addEventListener('keyup', updateTable)
+input.addEventListener('keyup', update)
 
 function updateTable() {
+    if(input.value.trim().length === 0)
+        return
+
     let table = $('#result').get(0)
     table.innerHTML = ''
 
-    if(vars.length === 0)
+    if(getUsedVars().length === 0) {
+        $('#result-title').get(0).innerText = `結果`
+        try {
+            table.innerHTML = `<span class="block code--block">${calculate()}</span>`
+        } catch (e) {
+            table.innerHTML = `<span class="block error--block">${e.message}</span>`
+        }
         return
+    }
 
     let tr = document.createElement('tr')
-    for(let _var of vars) {
+    for(let _var of getUsedVars()) {
         let th = document.createElement('th')
         th.innerText = _var
 
@@ -76,10 +90,18 @@ function updateTable() {
     tr.innerHTML += `<th>結果</th>`
     table.appendChild(tr)
 
-    let results = update()
+    let results = updateList(),
+        passed = 0
 
     for(let result of results) {
-        let tr = document.createElement('tr')
+        let tr = document.createElement('tr'), calcResult
+        try {
+            calcResult = calculate(result)
+        } catch (e) {
+            $('#result-title').get(0).innerText = '結果'
+            table.innerHTML = `<span class="block error--block">${e.message}</span>`
+            break
+        }
 
         for(let _var of Object.keys(result)) {
             let td = document.createElement('td')
@@ -87,17 +109,53 @@ function updateTable() {
             tr.appendChild(td)
         }
 
+        let td = document.createElement('td')
+        td.innerHTML = calcResult ?
+            `<i class="mdui-icon material-icons text--green">&#xe5ca;</i>` :
+            `<i class="mdui-icon material-icons text--red">&#xe14c;</i>`
+
+        if(calcResult)
+            passed++
+
+        tr.appendChild(td)
         table.appendChild(tr)
     }
+
+    $('#result-title').get(0).innerText = `結果 (${passed} / ${results.length - passed} / ${results.length})`
 }
 
-function update() {
+function updateList() {
     let result = {}, results = []
-    let for_list = vars.reduce((v, x) =>
-        v += `for(let i_${x} = 0; i_${x} <= 1; i_${x}++) {result['${x}'] = i_${x};if(Object.keys(result).length === vars.length && !results.find(x => JSON.stringify(x) === JSON.stringify(result))) results.push(Object.assign({}, result));`, '')
-    for(let _ of vars)
+    let for_list = getUsedVars().reduce((v, x) =>
+        v += `for(let i_${x} = 0; i_${x} <= 1; i_${x}++) {result['${x}'] = i_${x};if(Object.keys(result).length === getUsedVars().length && !results.find(x => JSON.stringify(x) === JSON.stringify(result))) results.push(Object.assign({}, result));`, '')
+    for(let _ of getUsedVars())
     for_list += '}'
     eval(for_list)
     results = results.sort((a, b) => Object.values(a).reduce((v, x) => v + x, 0) - Object.values(b).reduce((v, x) => v + x, 0))
     return results
+}
+
+function updateUsed() {
+    for(let _var of $('#vars').get(0).childNodes) {
+        if(getUsedVars().includes(_var.innerText) && !_var.classList.contains('used'))
+            return _var.classList.add('used')
+        if(!getUsedVars().includes(_var.innerText) && _var.classList.contains('used'))
+            _var.classList.remove('used')
+    }
+}
+
+function update() {
+    input.value = input.value.toUpperCase()
+    updateTable()
+    updateUsed()
+}
+
+function calculate(_vars = {}) {
+    for(let _var in _vars)
+        window[_var] = _vars[_var]
+    return eval(input.value)
+}
+
+function getUsedVars() {
+    return vars.filter(x => input.value.includes(x))
 }
